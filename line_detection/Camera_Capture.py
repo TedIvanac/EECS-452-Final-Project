f798ea24e233555cd39638f9738c102a4d2b02d1
Servando Garza <sergarza24@gmail.com>
2023-03-28 13:22:54 -0400

    Added test matrix. Need server RPI information.

--- line_detection/Camera_Capture.py
+++ line_detection/Camera_Capture.py
@@ -4,7 +4,7 @@ import numpy as np
 from Image_Process import Image_Process
 from Directions import Directions
 
-CAMERA_DIR = 4
+CAMERA_DIR = 0
 FPS = 15
 DELAY = int(1000/FPS)
 
@@ -23,6 +23,13 @@ class Camera_Capture():
         frame_count = 0
         prev_frame = np.ones((480, 640), dtype=np.uint8)*255
 
+        diff = np.array([[0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 1, 1, 1, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0]])
+
         while True:
             ret, frame = self.cap.read()
             if not ret:
@@ -33,9 +40,12 @@ class Camera_Capture():
             # self.image_processor._draw_contour(binary, frame)
 
             if frame_count % (FPS) == 0:
-                diff = self.image_processor._abs_diff(prev_frame, binary)
-                self.robot_commands._comm_command(diff)
+                # diff = self.image_processor._abs_diff(prev_frame, binary)
+                diff, distance, dir = self.robot_commands._comm_command(diff)
                 prev_frame = binary
+
+                if distance is not None and dir is not None:
+                    pass
             
             frame_count += 1
 
