72859a2709c5cfc8f0c263797ab1cd992366b7fd
Servando Garza <sergarza24@gmail.com>
2023-04-07 22:34:28 -0400

    Introduced setup function

--- line_detection/Camera_Capture.py
+++ line_detection/Camera_Capture.py
@@ -1,5 +1,4 @@
 import cv2 as cv
-import numpy as np
 
 import socket
 import struct
@@ -11,10 +10,10 @@ CAMERA_DIR = 4
 FPS = 15
 DELAY = int(1000/FPS)
 
-serverAddress = ('192.168.0.23',2222)
-
-bufferSize = 1024
-UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Server information
+SERVER_ADDRESS = ('192.168.0.23',2222)
+BUFFER_SIZE = 1024
+UDP_CLIENT = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 class Camera_Capture():
     
@@ -26,10 +25,25 @@ class Camera_Capture():
     # Return boolean based 
     def _camera_opened(self):
         return self.cap.isOpened()
+    
+    # Allows user to align camera until Enter key is pressed
+    def _setup(self):
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                return
+            
+            cv.imshow('Setup', frame)
+            if cv.waitKey(1) == 13:
+                break
 
+        cv.destroyWindow('Setup')
+
+    # Main function - iterating through frames and converting to binary images.
+    # Calculating distance and direction from binary image.
+    # Sending distance (float) and direction (int) to server.
     def _capture_frame(self):
         frame_count = 0
-        prev_frame = np.ones((480, 640), dtype=np.uint8)*255
         pause = False
 
         while True:
@@ -38,35 +52,32 @@ class Camera_Capture():
                 if not ret:
                     return
 
-                # cv.imshow('Default', frame)
                 binary = self.image_processor._gray2binary(frame)
                 cv.imshow('Binary', binary)
 
                 if frame_count % (FPS) == 0:
-                    # diff = self.image_processor._abs_diff(prev_frame, binary)
                     distance, dir = self.robot_commands._comm_command(binary)
-                    prev_frame = binary
 
                     if distance is not None and dir is not None:
                         message = struct.pack('fi', distance, dir)
-                        UDPClient.sendto(message, serverAddress)
+                        UDP_CLIENT.sendto(message, SERVER_ADDRESS)
 
-                
                 frame_count += 1
 
             key = cv.waitKey(DELAY)
+
+            # Pressing 'q' key will stop script
             if key == ord('q'):
                 print('Programming stopping')
                 break
 
+            # Pressing 'n' key will pause or unpause script
             if key == ord('n'):
                 pause = not pause
                 if pause:
                     print('Pausing')
                 else:
                     print('Unpausing')
-
-
         
         print('Destroying all Windows')
         self.cap.release()
